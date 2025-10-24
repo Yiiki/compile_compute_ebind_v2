e2083b920c732683ff98be367fb865d1b6e7ed3e
yzy <13971216897@163.com>
2025-10-24 16:50:56 +0800

    langfang

---
+++ compute_ebind.f90
@@ -0,0 +1,282 @@
+program compute_dipole
+implicit none
+character*256 :: frho,fcfg,fout
+
+integer*4 :: num_ele
+integer*4,allocatable :: type_list(:)
+real*8,allocatable :: zval_list(:)
+
+type config
+  integer*4 :: natom
+  integer*4,allocatable :: tatom(:),matom(:,:)
+  real*8 :: AL(3,3)
+  real*8,allocatable :: xatom(:,:)
+end type config
+
+type(config) :: ac
+
+integer*4 :: ipln, num_pln, num_lat, iatom, n1,n2,n3
+real*8 :: dipole,xtmp,xcut,dvol,a_au
+real*8,allocatable :: dipole_layer(:), zval(:), &
+                      dipole_rho(:),rho(:,:,:)
+
+real*8 :: tot_zval
+real*8 :: AL(3,3), AL_out(3,3)
+real*8,parameter :: one_third=1.d0/3.d0
+integer*4 :: new_unit,i,j,k,ierr,nnodest
+real*8,allocatable :: rho_out(:,:,:)
+real*8 :: V_corner,V_bulk,E_6,E6_1,E6_2,E6_3,Evbm,E_bind,E_bind2,E_d, E_7, Ecbm, E_e
+real*8,allocatable :: occ(:)
+integer*4 :: num_band, ith_eigen, ith_bedge
+real*8,parameter :: E_AU=27.211386d0
+logical :: ist
+character*256 :: band_type
+real*8 :: sign_e
+logical :: iqst
+integer*4 :: n1_ref, n2_ref, n3_ref
+integer*4 :: n1_tmp, n2_tmp, n3_tmp
+character*256 :: dir_ref,fref,dir_tmp,ftmp,m33
+
+call read_parameter("parameter.input",m33)
+
+select case (trim(band_type))
+  case ('VBM')
+    sign_e=1.d0
+  case ('CBM')
+    sign_e=-1.d0
+  case DEFAULT
+    sign_e=1.d0
+    write(6,*) "band_type = VBM or CBM, please check", trim(band_type)
+    stop
+end select
+
+
+write(fref,'(A,A)') trim(dir_ref),"/OUT.VR"
+
+inquire(file=trim(fref),exist=iqst)
+
+if(.not.iqst) then
+  write(6,*) trim(fref), "not exist, stop"
+  stop
+end if
+
+call read_rho(fref,rho,AL,n1,n2,n3,nnodest)
+V_bulk=rho(n1_ref, n2_ref, n3_ref )*E_AU
+
+
+
+write(ftmp,'(A,A)') trim(dir_tmp),"/OUT.VR"
+
+inquire(file=trim(ftmp),exist=ist)
+if(ist) then
+call read_rho(ftmp,rho,AL,n1,n2,n3,nnodest)
+V_corner=rho(n1_tmp, n2_tmp, n3_tmp)*E_AU
+else
+write(6,*) "OUT.VR not found, use V_bulk by default"
+V_corner=V_bulk
+end if
+
+write(fref,'(A,A)') trim(dir_ref),"/OUT.OCC"
+
+inquire(file=trim(fref),exist=iqst)
+
+if(.not.iqst) then
+  write(6,*) trim(fref), "not exist, stop"
+  stop
+end if
+
+call read_occ(fref,occ)
+! ith_bedge=1024
+E_e=occ(ith_bedge)
+E_d=occ(ith_bedge+nint(sign_e))
+
+write(ftmp,'(A,A)') trim(dir_tmp),"/OUT.OCC"
+
+inquire(file=trim(ftmp),exist=iqst)
+
+if(.not.iqst) then
+  write(6,*) trim(ftmp), "not exist, stop"
+  stop
+end if
+
+
+call read_occ(ftmp,occ)
+! ith_eigen=1024
+E_6=occ(ith_eigen)
+E_7=occ(ith_eigen+nint(sign_e))
+
+E_bind=sign_e*(E_6-V_corner+V_bulk-E_e) ! suggested by LWW
+
+E_bind2 = sign_e*(E_6 - (E_7 - (E_d - E_e) )) ! use the unocc orbit as reference
+
+call write_output()
+
+contains
+subroutine read_occ(file,occ)
+implicit none
+integer*4 :: nline,iE,iline,ios
+real*8 :: f
+character(len=*) :: file
+real*8,allocatable :: occ(:)
+print *, "we only address single K-point ! no spin !"
+nline=0
+  open(newunit=new_unit,file=trim(file))
+  rewind(new_unit)
+loop_line : do
+  read(new_unit,*,iostat=ios) 
+  if(ios.ne.0) exit loop_line
+  nline=nline+1
+end do loop_line
+  close(new_unit)
+print *, "total line", nline
+if(allocated(occ)) deallocate(occ)
+allocate(occ(nline-2))
+  open(newunit=new_unit,file=trim(file))
+  rewind(new_unit)
+  read(new_unit,*,iostat=ios) 
+  read(new_unit,*,iostat=ios) 
+do iline=1,nline-2
+  read(new_unit,*,iostat=ios) iE, occ(iline), f 
+end do
+  close(new_unit)
+end subroutine read_occ
+
+subroutine read_parameter(file,m33)
+implicit none
+character(len=*) :: file
+!real*8 :: m33
+character(len=*) :: m33
+open(newunit=new_unit,file=trim(file))
+rewind(new_unit)
+read(new_unit,'(a)') dir_ref
+read(new_unit,*) n1_ref, n2_ref, n3_ref
+read(new_unit,*) ith_bedge  ! what type of this ith bedge is
+read(new_unit,*) band_type  ! what type of this ith bedge is
+read(new_unit,'(a)') dir_tmp
+read(new_unit,*) n1_tmp, n2_tmp, n3_tmp
+read(new_unit,*) ith_eigen
+read(new_unit,'(A)') m33
+close(new_unit)
+write(6,*) "band_type = ", trim(band_type)
+write(6,*) "band_edge = ", ith_bedge
+write(6,*) "defect ei = ", ith_eigen
+write(6,*) "mii       = ", m33
+end subroutine read_parameter
+
+subroutine write_output()
+implicit none
+open(newunit=new_unit,file="data.out")
+rewind(new_unit)
+write(new_unit,190) "mii", "E_bind2 (eV)", "E_bind(eV)=", "[(E_6- E_e) +", "(V_bulk-V_corner)]*", "sign_e"
+write(new_unit,191) trim(m33), E_bind2, E_bind, E_6-E_e, V_bulk-V_corner, sign_e   
+191 format(1x,A19,5(1x,E19.10))
+190 format(6(1x,A19   ))
+close(new_unit)
+end subroutine write_output
+
+subroutine read_rho(file,rho,AL,n1,n2,n3,nnodest)
+implicit none
+character(len=*),intent(in) :: file
+real*8,allocatable,intent(out) :: rho(:,:,:)
+integer*4,intent(out) :: n1,n2,n3
+real*8,intent(out) :: AL(3,3)
+integer*4,intent(out) :: nnodest
+integer*4 :: nr, nr_n, iread, ii, jj, i, j, k
+real*8,allocatable :: vr_tmp(:)
+open(13,file=trim(file),form="unformatted",&
+    status='old',action='read',iostat=ierr)
+rewind(13)
+read(13) n1 ,n2 ,n3 ,nnodest
+read(13) AL
+if(allocated(rho)) deallocate(rho)
+allocate(rho(n1,n2,n3))
+nr=n1*n2*n3
+nr_n=(n1*n2*n3)/nnodest
+allocate(vr_tmp(nr_n))
+do iread=1,nnodest
+   read(13) vr_tmp
+   do ii=1,nr_n
+      jj=ii+(iread-1)*nr_n
+      i=(jj-1)/(n2*n3)+1
+      j=(jj-1-(i-1)*n2*n3)/n3+1
+      k=jj-(i-1)*n2*n3-(j-1)*n3
+      rho(i,j,k)=vr_tmp(ii)
+   enddo
+enddo
+close(13)
+deallocate(vr_tmp)
+end subroutine read_rho
+
+subroutine outp_rho(file,rho,AL,n1,n2,n3,nnodest)
+implicit none
+character(len=*),intent(in) :: file
+real*8,allocatable,intent(in) :: rho(:,:,:)
+integer*4,intent(in) :: n1,n2,n3
+real*8,intent(in) :: AL(3,3)
+integer*4,intent(in) :: nnodest
+integer*4 :: nr, nr_n, iread, ii, jj, i, j, k, nnodes_tmp
+real*8,allocatable :: vr_tmp(:)
+
+nnodes_tmp=nnodest
+open(13,file=trim(file),form="unformatted")
+rewind(13)
+write(13) n1,n2,n3,nnodes_tmp
+write(13) AL
+nr_n=(n1*n2*n3)/nnodes_tmp
+allocate(vr_tmp(nr_n))
+do iread=1,nnodes_tmp
+   do ii=1,nr_n
+      jj=ii+(iread-1)*nr_n
+      i=(jj-1)/(n2*n3)+1
+      j=(jj-1-(i-1)*n2*n3)/n3+1
+      k=jj-(i-1)*n2*n3-(j-1)*n3
+      vr_tmp(ii)=rho(i,j,k)
+   enddo
+   write(13) vr_tmp
+enddo
+close(13)
+deallocate(vr_tmp)
+
+end subroutine outp_rho
+
+subroutine add_rho()
+implicit none
+do k=1,n3
+do j=1,n2
+do i=1,n1
+! rho(i,j,k)=rho(i,j,k)-2.392108858132828d-05*(i-1.d0-192)
+! rho(i,j,k)=rho(i,j,k)-0.035857327586521d-3*(i-1.d0-192)
+! rho(i,j,k)=rho(i,j,k)-3*0.035857327586521d-3*(i-1.d0-192)
+! rho(i,j,k)=rho(i,j,k)-2*0.035857327586521d-3*(i-1.d0-192)
+! rho(i,j,k)=rho(i,j,k)-1.5d0*0.035857327586521d-3*(i-1.d0-192)
+! rho(i,j,k)=rho(i,j,k)-1.25d0*0.035857327586521d-3*(i-1.d0-192)
+rho_out(i,j,k)=rho(i,j,k)
+if(i.gt.1) rho_out(2*n1+2-i,j,k)=rho(i,j,k)
+end do
+end do
+end do
+end subroutine add_rho
+
+subroutine read_config(file,ac)
+implicit none
+character(len=*),intent(in) :: file
+type(config),intent(out) :: ac
+integer*4 :: new_unit,iatom
+open(newunit=new_unit,file=trim(file))
+rewind(new_unit)
+read(new_unit,*) ac%natom
+read(new_unit,*) ! LATTICE
+read(new_unit,*) ac%AL(:,1)
+read(new_unit,*) ac%AL(:,2)
+read(new_unit,*) ac%AL(:,3)
+read(new_unit,*) ! POSITION
+allocate(ac%tatom(ac%natom),&
+ac%xatom(3,ac%natom),&
+ac%matom(3,ac%natom))
+do iatom=1,ac%natom
+read(new_unit,*) ac%tatom(iatom),ac%xatom(1:3,iatom),ac%matom(1:3,iatom) 
+end do
+close(new_unit)
+end subroutine read_config
+
+end program compute_dipole
---
+++ fsm2occ.f90
@@ -0,0 +1,46 @@
+program fsm2occ
+implicit none
+integer*4 :: new_unit,ios,ntot,nhig,pos,i
+character*256 :: str_key, str
+real*8,allocatable :: eigs(:)
+real*8 :: occ
+str_key="eigen energies, in ev"
+call getarg(1,str)
+read(str,*) ntot
+if(ntot.ne.10) then
+  write(6,*) "quick test, only work for 10 eigens"
+  stop
+end if
+allocate(eigs(ntot))
+call getarg(2,str)
+read(str,*) nhig
+
+open(newunit=new_unit,file="out")
+rewind(new_unit)
+do 
+read(new_unit,'(A)',iostat=ios) str
+if(ios.ne.0) exit
+
+pos=index(str,trim(str_key))
+if(pos.gt.0) then
+read(new_unit,*) eigs(1:5)
+read(new_unit,*) eigs(6:10)
+end if
+
+end do
+close(new_unit)
+
+open(newunit=new_unit,file="OUT.OCC.FSM")
+rewind(new_unit)
+write(new_unit,*) "KPOINTS      1:    0.0000    0.0000    0.0000"
+write(new_unit,*) " NO.   ENERGY(eV) OCCUPATION"
+do i=1,ntot
+occ=2.d0
+if(i.gt.nhig) occ=0.d0
+write(new_unit,141) i, eigs(i), occ
+141 format(I7,F12.4,F10.5)
+end do
+close(new_unit)
+
+stop
+end program fsm2occ
